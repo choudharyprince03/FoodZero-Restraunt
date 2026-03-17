5130944b3683097c4394764be8d52d5c1a0aecf1
Prince Choudhary <prince.1978choudhary@gmail.com>
2026-03-17 16:47:50 +0530

    env update

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const sheetUrl = process.env.VITE_SHEET_LINK || "https://script.google.com/macros/s/AKfycbwpZdhqMlXfrNRZJKuGYRFgAgBGaFKgxCTjx_jPaId9ElnwdcWaheRv7NTVqNRfE-OwzA/exec";
+
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   server: {
     proxy: {
       "/api/booking": {
-        target: "https://script.google.com",
+        target: new URL(sheetUrl).origin,
         changeOrigin: true,
         rewrite: (path) =>
           path.replace(
             "/api/booking",
-            "/macros/s/AKfycbwpZdhqMlXfrNRZJKuGYRFgAgBGaFKgxCTjx_jPaId9ElnwdcWaheRv7NTVqNRfE-OwzA/exec"
+            new URL(sheetUrl).pathname
           ),
       },
     },
